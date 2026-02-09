4412ced2e4704a92ca7bc1bfc0c2cb6f68fbac2f
shraddha09107 <shraddha09singh06@gmail.com>
2026-02-09 09:57:52 +0530

    Add servo control functionality and update configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
+#include <Servo.h>
+Servo myServo;  // create servo object to control a servo
+#define SERVO_PIN 9
 void setup() {
 // write your initialization code here
+    myServo.attach(9);  // attaches the servo on pin 9 to the servo object
+    Serial.begin(9600);
 }
 
 void loop() {
 // write your code here
+    //write a loop code
+    for (int pos = 0; pos <= 180; pos += 40) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myServo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+    }
+    for (int pos = 180; pos >= 0; pos -= 90)
+    {
+        // goes from 180 degrees to 0 degrees
+        myServo.write(pos);              //
+        delay(15);                       // waits 15ms for the servo to reach the position
+    }
+
+
+
 }
